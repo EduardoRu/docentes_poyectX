42feced8ec29cbd8c80a0f2c1a6de8ed8d203c1a
Eduardo Ruelas Cortés <eduruelas13@gmail.com>
2023-03-13 12:39:42 -0600

    ajustes generales

--- aceptados.php
+++ aceptados.php
@@ -100,7 +100,7 @@ if (isset($_SESSION['id']) && $_SESSION['nombre']) {
                         ?>
                         <!-- Page Heading -->
                         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                            <h1 class="h3 mb-0 text-gray-800">Adminstración de candiatos a docentes</h1>
+                            <h1 class="h3 mb-0 text-gray-800">Adminstración de aceptados</h1>
                         </div>
 
                         <div class="row">
